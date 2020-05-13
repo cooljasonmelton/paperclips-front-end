f7a852cad6c70f78f13903851eb47923b7af5fcb
cooljasonmelton <cooljasonmelton>
2020-05-13 13:42:05 -0500

    working

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Title = () => {
     return (
         <div className="title-container">
-            <img className="title-pic" src={process.env.PUBLIC_URL + '/paperclips.jpeg'} alt="paperclips"/>
+            <img className="title-pic" 
+                src={process.env.PUBLIC_URL + '/paperclips.jpeg'} 
+                alt="paperclips"
+                />
             <h1 className="title-text">paperclips</h1>
         </div>
     )
