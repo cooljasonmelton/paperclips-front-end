5015e7a0c1d940d7e7cf3ab620a8896258998283
cooljasonmelton <cooljasonmelton>
2020-05-12 19:40:22 -0500

    set up

---
+++ src/components/TestComp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TestComp = () => {
+    return (
+        <div className="test1">
+            what up
+            <div className="test2">
+                What Up?
+                <div className="test3">
+                    <h1>WHAT UP</h1>
+                    <input type='area'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TestComp
